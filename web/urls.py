bef94e120a66138f043648d5d297d6459d3633d4
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-20 11:19:23 +0330

    added help path and better import

--- web/urls.py
+++ web/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, register_converter
-from .views import index, about, export_group_link
+from . import views
 from .path_convertor import UnicodeSlug
 
 # register our slug (unicode slug)
 register_converter(UnicodeSlug, 'uSlug')
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('link/<uSlug:slug>/', export_group_link, name='get_group_link'),
-    path('about', about, name='about'),
+    path('', views.index, name='index'),
+    path('link/<uSlug:slug>/', views.export_group_link, name='get_group_link'),
+    path('help/', views.help, 'help'),
+    path('about', views.about, name='about'),
 ]
