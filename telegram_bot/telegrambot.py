8e2d2076a2acce6c0a5fab0a03a2695f3516b250
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-20 18:13:03 +0330

    Better error handling

--- telegram_bot/telegrambot.py
+++ telegram_bot/telegrambot.py
@@ -1,7 +1,8 @@
 from telegram.ext import CommandHandler, MessageHandler, Filters
-from telegram import TelegramError, Bot
+from telegram import Bot
+from telegram.error import (TelegramError, Unauthorized, BadRequest)
 from django_telegrambot.apps import DjangoTelegramBot
-from .errors import PermissionDenied
+from .errors import (PermissionDenied, BotIsKickedOut, ChatNotFound)
 from . import commands, app_settings
 import logging
 
@@ -11,13 +12,36 @@ logging.basicConfig(filename='logs/telegram-bot.log', level=logging.INFO)
 
 
 def get_group_link(chat_id):
+    """Get chat invite link from telegram
+    param:
+        chat_id: group chat id (required)
+    return:
+        group link
+    raises:
+        BotIsKickedOut,
+        ChatNotFound,
+        Permission Denied"""
+
+    if chat_id is None or not isinstance(chat_id, int):
+        raise ValueError('chat_id is invalid.')
+
     bot = Bot(app_settings.BOT_TOKEN)
     try:
         logger.info('Getting chat invite link. chat_id'.format(chat_id))
         return bot.export_chat_invite_link(chat_id)
-    except TelegramError as err:
-        logger.warning(err)
-        raise PermissionDenied(err.message)
+
+    except Unauthorized as un_auth:
+        if un_auth.message == 'Forbidden: bot was kicked from the supergroup chat':
+            raise BotIsKickedOut()
+        else:
+            raise
+
+    except BadRequest as bad_req:
+        if bad_req.message == 'Not enough rights to export chat invite link':
+            raise PermissionDenied()
+        if bad_req.message == 'Chat not found':
+            raise ChatNotFound()
+        raise
 
 
 def change_name(chat_id, new_name):
