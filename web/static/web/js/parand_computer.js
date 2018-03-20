789a28905648edc939f1acb1def552d6e02f446f
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-20 22:20:38 +0330

    added clickable row option
    + fixed github icon on small devices

--- web/static/web/js/parand_computer.js
+++ web/static/web/js/parand_computer.js
@@ -1,11 +1,8 @@
 'use strict'
 
-function groupSelected () {
-  // window.location.href = './?name=ازمایشگاه'
-  $('my-card').first.innerHTML = 'amir'
-}
-
 // initial jquery
 $(document).ready(function () {
-
+    $(".clickable-row").click(function() {
+            window.location = $(this).data("href");
+        });
 })
