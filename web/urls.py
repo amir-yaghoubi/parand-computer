1e3b788714c9e91acb3e971d278c1eb739339774
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-20 11:55:18 +0330

    fixed typo issue

--- web/urls.py
+++ web/urls.py
@@ -8,6 +8,6 @@ register_converter(UnicodeSlug, 'uSlug')
 urlpatterns = [
     path('', views.index, name='index'),
     path('link/<uSlug:slug>/', views.export_group_link, name='get_group_link'),
-    path('help/', views.help_view, 'help'),
+    path('help', views.help_view, name='help'),
     path('about', views.about, name='about'),
 ]
