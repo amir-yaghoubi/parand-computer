aa960ab2f88e3154308128a8138d3ae9b434a513
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-21 09:45:13 +0330

    Better exception handling
    removed exception handling in view and moving them in middleware class
    web.middleware.TelegramErrorMiddleware

--- web/views.py
+++ web/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from telegram_bot.telegrambot import get_group_link, pin_message as pin_message_telegram
-from telegram_bot.errors import (PermissionDenied, ChatNotFound, BotIsKickedOut)
+from telegram_bot.telegrambot import get_group_link
 from .models import Group
 
 
@@ -30,34 +28,15 @@ def index(request):
 
 
 def export_group_link(request, slug):
+    # پیدا کردن گروه بر اساس اسلاگ و صفحه 404 در صورت عدم پیدا شدن
     group = get_object_or_404(Group, slug=slug)
-    try:
-        link = get_group_link(chat_id=group.chat_id)
-        return redirect(link)
+    request.group = group
+    # دریافت لینک گروه از تلگرام که ممکن است دچار Exception شود
+    # که رسیدگی به این خطاها توسط Middleware نوشته شده انجام میگردد
+    # رجوع شود به web.middleware.TelegramErrorMiddleware
+    link = get_group_link(chat_id=group.chat_id)
 
-    except PermissionDenied:
-        group.active = False
-        group.save()
-        context = {'error': 'عدم دسترسی به لینک گروه',
-                   'error_text': 'ادمین گروه درخواستی شما دسترسی به لینک گروه را از ربات ما گرفته است، به همین خاطر گروه غیر فعال شد.'}
-        return render(request, 'web/errorPage.html', context)
-
-    except ChatNotFound:
-        group.delete()
-        context = {'error': 'گروه درخواستی شما یافت نشد.',
-                   'error_text': 'احتمالا گروه حذف گردیده است، به هماین دلیل این گروه از لیست گروه‌های سایت ما خارج شد.'}
-        return render(request, 'web/errorPage.html', context)
-
-    except BotIsKickedOut:
-        group.delete()
-        context = {'error': 'ربات ما از گروه درخواستی شما حذف شده است!',
-                   'error_text': 'به همین دلیل گروه از سایت حذف می‌شود. در صورتی که فکر میکنید اشتباهی رخ داده است با ادمین تماس بگیرید.'}
-        return render(request, 'web/errorPage.html', context)
-
-
-def pin_msg(request):
-    pin_message_telegram(chat_id=-1001157994007, msg='امتحان پین توسط ربات')
-    return HttpResponse('پیام ارسال شد')
+    return redirect(link)
 
 
 def about(request):
