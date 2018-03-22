6610cad81192b68ce4bf35a3eff952faf754f5f1
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-22 09:47:02 +0430

    approve group

--- panel/forms.py
+++ panel/forms.py
@@ -1,2 +1,8 @@
 from django.forms import ModelForm
+from web.models import Group
 
+
+class ApproveGroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ['title', 'category', 'teacher', 'active']
--- panel/views.py
+++ panel/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.urls import reverse_lazy
-from django.views.generic import FormView, RedirectView, ListView
+from django.views.generic import FormView, RedirectView, ListView, CreateView
 from django.shortcuts import HttpResponse, HttpResponseRedirect, get_object_or_404, redirect
 from web.models import PendingGroup, Group, Teacher
+from .forms import ApproveGroupForm
+from datetime import datetime
 
 
 def index(request):
@@ -39,8 +41,30 @@ class LogoutView(RedirectView):
         return super(LogoutView, self).get(request, *args, **kwargs)
 
 
+class ApproveGroupView(CreateView):
+    template_name = 'panel/form.html'
+    form_class = ApproveGroupForm
+    success_url = reverse_lazy('panel:index')
+
+    def get_initial(self):
+        pending = get_object_or_404(PendingGroup, slug=self.kwargs['slug'])
+        print(pending)
+        return {'group': pending}
+
+    def form_valid(self, form):
+        pending = get_object_or_404(PendingGroup, slug=self.kwargs['slug'])
+        form.instance.chat_id = pending.chat_id
+        form.instance.admin_id = pending.admin_id
+        form.instance.admin_username = pending.admin_username
+        form.instance.created_date = datetime.now()
+        form.instance.members = 12  # get from telegram
+        pending.delete()
+
+        return super(ApproveGroupView, self).form_valid(form)
+
+
 def approve_group(request, slug):
-    '''Place holder'''
+    """Place Holder"""
     pending = get_object_or_404(PendingGroup, slug=slug)
     teacher = Teacher.objects.get(pk=1)
     Group.objects.create(title=pending.title,
@@ -49,6 +73,7 @@ def approve_group(request, slug):
                          category='T', active=True, teacher=teacher)
     pending.delete()
     return redirect('panel:index')
+
 
 def placeholder(request):
     pass
