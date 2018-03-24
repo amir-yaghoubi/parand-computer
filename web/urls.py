8a421462306d71cf9c461eb577f6c91504b55812
Amir yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-25 00:33:51 +0430

    removed url pattern for gets group link

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'web'
 urlpatterns = [
     path('', views.IndexPage.as_view(), name='index'),
-    path('link/<groupSlug:slug>/', views.export_group_link, name='get_group_link'),
     path('help', views.help_view, name='help'),
     path('about', views.about, name='about'),
 ]
